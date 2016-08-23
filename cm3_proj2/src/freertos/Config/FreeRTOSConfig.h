21942fcbcaddfbb82eaf12e2c91e326eec943a9b
daqingzhang <daqingzhang@rdamicro.com>
2016-08-23 23:34:18 +0800

    update cm3_proj2 makefile for freeRTOS. Now it can compile asm files successfully.

--- cm3_proj2/src/freertos/Config/FreeRTOSConfig.h
+++ cm3_proj2/src/freertos/Config/FreeRTOSConfig.h
@@ -88,10 +88,10 @@ extern uint32_t SystemCoreClock;
 //extern const uint32_t SystemFrequency;
 
 #define configUSE_PREEMPTION			1
-#define configUSE_IDLE_HOOK				1
-#define configUSE_TICK_HOOK				0
-#define configCPU_CLOCK_HZ				( SystemCoreClock )
-#define configTICK_RATE_HZ				( ( TickType_t ) 100 )
+#define configUSE_IDLE_HOOK			1
+#define configUSE_TICK_HOOK			0
+#define configCPU_CLOCK_HZ			( SystemCoreClock )
+#define configTICK_RATE_HZ			( ( TickType_t ) 100 )
 #define configMAX_PRIORITIES			( 50 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 130 )
 #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 10 * 1024 ) )
@@ -99,21 +99,21 @@ extern uint32_t SystemCoreClock;
 #define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			1
-#define configUSE_MUTEXES				1
+#define configUSE_MUTEXES			1
 #define configQUEUE_REGISTRY_SIZE		8
-#define configCHECK_FOR_STACK_OVERFLOW	2
+#define configCHECK_FOR_STACK_OVERFLOW		2
 #define configUSE_RECURSIVE_MUTEXES		1
-#define configUSE_MALLOC_FAILED_HOOK	1
-#define configUSE_APPLICATION_TASK_TAG	0
-#define configUSE_COUNTING_SEMAPHORES	1
-#define configGENERATE_RUN_TIME_STATS	0
+#define configUSE_MALLOC_FAILED_HOOK		1
+#define configUSE_APPLICATION_TASK_TAG		0
+#define configUSE_COUNTING_SEMAPHORES		1
+#define configGENERATE_RUN_TIME_STATS		0
 
 /* Co-routine definitions. */
-#define configUSE_CO_ROUTINES 		    0
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+#define configUSE_CO_ROUTINES 		    	0
+#define configMAX_CO_ROUTINE_PRIORITIES 	( 2 )
 
 /* Software timer definitions. */
-#define configUSE_TIMERS				0
+#define configUSE_TIMERS			0
 #define configTIMER_TASK_PRIORITY		( 2 )
 #define configTIMER_QUEUE_LENGTH		10
 #define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE * 2 )
@@ -122,12 +122,12 @@ extern uint32_t SystemCoreClock;
 to exclude the API function. */
 #define INCLUDE_vTaskPrioritySet		1
 #define INCLUDE_uxTaskPriorityGet		1
-#define INCLUDE_vTaskDelete				1
-#define INCLUDE_vTaskCleanUpResources	1
+#define INCLUDE_vTaskDelete			1
+#define INCLUDE_vTaskCleanUpResources		1
 #define INCLUDE_vTaskSuspend			1
 #define INCLUDE_vTaskDelayUntil			1
-#define INCLUDE_vTaskDelay				1
-#define INCLUDE_eTaskGetState           1
+#define INCLUDE_vTaskDelay			1
+#define INCLUDE_eTaskGetState           	1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
@@ -153,11 +153,11 @@ to all Cortex-M ports, and do not rely on any particular library functions. */
 /* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
 See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
 #define configMAX_SYSCALL_INTERRUPT_PRIORITY 	( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
-	
+
 /* Normal assert() semantics without relying on the provision of an assert.h
 header file. */
 #define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }	
-	
+
 /* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
 standard names. */
 #if OS_EN

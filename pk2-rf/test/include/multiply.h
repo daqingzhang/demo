c9baa71ca667d3d1408e4e634eff242e043c54c0
daqingzhang <daqingzhang@rdamicro.com>
2017-06-15 17:55:03 +0800

    remove pk2-rf proj

--- pk2-rf/test/include/multiply.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MULTIPLY_H__
-#define __MULTIPLY_H__
-
-int data32_sum(int max);
-int hex2asc(unsigned int hex, char *asc);
-unsigned int asc2hex(const char *asc);
-int multiply( int x, int y );
-#endif
